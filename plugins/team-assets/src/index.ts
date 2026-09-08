93972e66ed06e20f67be2ba49763f54964e037e9
Developer FL <developer-fl@fufulabs.com>
2026-09-08 16:22:22 +0800

    feat: add team-assets package with i18n strings and register in platform
    - Created team-assets plugin with en.json translations matching team plugin strings
    - Registered team-assets in rush.json
    - Added addStringsLoader for team plugin in dev/prod/src/platform.ts
    - Added addLocation for team-resources to load TeamManagement component
    - Added teamId import to platform.ts with teamIdForStrings alias

---
+++ plugins/team-assets/src/index.ts
@@ -0,0 +1,10 @@
+//
+// Copyright 2025 Hardcore Engineering Inc.
+//
+
+import { loadMetadata } from "@hcengineering/platform"
+import teamPlugin from "@hcengineering/team"
+
+loadMetadata(teamPlugin.icon, {
+  Team: ""
+})
