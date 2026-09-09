2e88122167be30122c8b8fc34e5e38b1647c80ee
Developer FL <developer-fl@fufulabs.com>
2026-09-09 09:49:13 +0800

    fix: remove pencil glyph from group row
    Matches Spaces exactly - clean [label]...[Members pill] layout without extra edit glyph.

--- plugins/group-resources/src/components/GroupManagement.svelte
+++ plugins/group-resources/src/components/GroupManagement.svelte
@@ -61,11 +61,6 @@
                   size="large"
                 />
               </div>
-              <button class="edit-pencil" on:click={() => handleEdit(group)} title="Edit group">
-                <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-                  <path d="M11.5 1.5C11.6326 1.5 11.7598 1.55268 11.8536 1.64645L14.3536 4.14645C14.4473 4.24022 14.5 4.36739 14.5 4.5C14.5 4.63261 14.4473 4.75979 14.3536 4.85355L5.85355 13.3536C5.75979 13.4473 5.63261 13.5 5.5 13.5H3C2.72386 13.5 2.5 13.2761 2.5 13V10.5C2.5 10.3674 2.55268 10.2402 2.64645 10.1464L10.1464 2.64645C10.2402 2.55268 10.3674 2.5 10.5 2.5H11.5ZM10.5 3.60355L3.5 10.6036V12.5H5.39645L12.3964 5.5L10.5 3.60355ZM13.5 4.5L11.5 2.5L12.6464 3.64645L13.5 4.5Z" />
-                </svg>
-              </button>
             </div>
           {/each}
         </div>
@@ -91,16 +86,7 @@
     text-overflow: ellipsis;
     white-space: nowrap;
   }
-  .edit-pencil {
-    background: none;
-    border: none;
-    cursor: pointer;
-    padding: 0.25rem;
-    color: var(--theme-caption-color);
-    display: inline-flex;
-    align-items: center;
-    &:hover { color: var(--theme-accent-color); }
-  }
+
   .content {
     margin: 2rem 3.25rem;
   }
